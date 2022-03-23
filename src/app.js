843225329359f41b054309424779948592e40d08
Melwyn Turbant <melwynt@gmail.com>
2022-03-23 19:21:23 +0100

    :rocket: Create layout

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  return <div>Hello, World!</div>;
+  return (
+    <>
+      <div className="sidebar">Sidebar</div>
+      <header className="header">Header</header>
+      <main className="realtors">Top 3 realtors</main>
+      <section className="features">Features</section>
+      <div className="story__pictures">Story Pictures</div>
+      <div className="story__content">Story Content</div>
+      <section className="homes">Homes</section>
+      <section className="gallery">Gallery</section>
+      <footer className="footer">Footer</footer>
+    </>
+  );
 };
 
 export default App;
