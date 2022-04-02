49fd24f8ba110a8f2308bba55220a874e5e579ae
Melwyn Turbant <melwynt@gmail.com>
2022-04-02 06:29:50 +0200

    :sparkles: Create Features Section

--- src/app.js
+++ src/app.js
@@ -1,12 +1,83 @@
 import React, { useState } from 'react';
 
+import sprite from '../images/sprite.svg';
+
 const App = () => {
   return (
     <>
       <div className="sidebar">Sidebar</div>
       <header className="header">Header</header>
       <main className="realtors">Top 3 realtors</main>
-      <section className="features">Features</section>
+      <section className="features">
+        <div className="feature">
+          <svg className="feature__icon">
+            <use href={sprite + '#icon-global'} />
+          </svg>
+          <h4 className="heading-4 heading-4--dark">
+            World's best luxury homes
+          </h4>
+          <p className="feature__text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+            distinctio necessitatibus pariatur voluptatibus.
+          </p>
+        </div>
+        <div className="feature">
+          <svg className="feature__icon">
+            <use href={sprite + '#icon-trophy'} />
+          </svg>
+          <h4 className="heading-4 heading-4--dark">
+            Only the best properties
+          </h4>
+          <p className="feature__text">
+            Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus
+            rerum sed a eligendi aut quia.
+          </p>
+        </div>
+        <div className="feature">
+          <svg className="feature__icon">
+            <use href={sprite + '#icon-map-pin'} />
+          </svg>
+          <h4 className="heading-4 heading-4--dark">
+            All homes in top locations
+          </h4>
+          <p className="feature__text">
+            Tenetur distinctio necessitatibus pariatur voluptatibus quidem
+            consequatur harum.
+          </p>
+        </div>
+        <div className="feature">
+          <svg className="feature__icon">
+            <use href={sprite + '#icon-key'} />
+          </svg>
+          <h4 className="heading-4 heading-4--dark">New home in one week</h4>
+          <p className="feature__text">
+            Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor
+            sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className="feature">
+          <svg className="feature__icon">
+            <use href={sprite + '#icon-presentation'} />
+          </svg>
+          <h4 className="heading-4 heading-4--dark">Top 1% realtors</h4>
+          <p className="feature__text">
+            Quidem consequatur harum, voluptatum mollitia quae. Tenetur
+            distinctio necessitatibus pariatur voluptatibus.
+          </p>
+        </div>
+        <div className="feature">
+          <svg className="feature__icon">
+            <use href={sprite + '#icon-lock'} />
+          </svg>
+          <h4 className="heading-4 heading-4--dark">
+            Secure payments on Nexter
+          </h4>
+          <p className="feature__text">
+            Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia
+            quae.
+          </p>
+        </div>
+      </section>
       <div className="story__pictures">Story Pictures</div>
       <div className="story__content">Story Content</div>
       <section className="homes">Homes</section>
