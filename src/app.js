f3c1abb1f3261a74e81a5883c4f960da5cf61928
Melwyn Turbant <melwynt@gmail.com>
2022-04-21 00:32:52 +0200

    :rocket: Create home section

--- src/app.js
+++ src/app.js
@@ -80,7 +80,242 @@ const App = () => {
       </section>
       <div className="story__pictures">Story Pictures</div>
       <div className="story__content">Story Content</div>
-      <section className="homes">Homes</section>
+      <section className="homes">
+        <div className="home">
+          <img
+            src={require('../images/house-1.jpeg')}
+            alt="House 1"
+            className="home__img"
+          />
+          <svg className="home__like">
+            <use href={sprite + '#icon-heart-full'} />
+          </svg>
+          <h5 className="home__name">Beautiful Family House</h5>
+          <div className="home__location">
+            <svg>
+              <use href={sprite + '#icon-map-pin'} />
+            </svg>
+            <p>USA</p>
+          </div>
+          <div className="home__rooms">
+            <svg>
+              <use href={sprite + '#icon-profile-male'} />
+            </svg>
+            <p>5 rooms</p>
+          </div>
+          <div className="home__area">
+            <svg>
+              <use href={sprite + '#icon-expand'} />
+            </svg>
+            <p>
+              325 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <svg>
+              <use href={sprite + '#icon-key'} />
+            </svg>
+            <p>$1,200,000</p>
+          </div>
+          <button className="cta">Contact realtor</button>
+        </div>
+        {/* end home 1 */}
+        <div className="home">
+          <img
+            src={require('../images/house-2.jpeg')}
+            alt="House 2"
+            className="home__img"
+          />
+          <svg className="home__like">
+            <use href={sprite + '#icon-heart-full'} />
+          </svg>
+          <h5 className="home__name">Modern Glass Villa</h5>
+          <div className="home__location">
+            <svg>
+              <use href={sprite + '#icon-map-pin'} />
+            </svg>
+            <p>Canada</p>
+          </div>
+          <div className="home__rooms">
+            <svg>
+              <use href={sprite + '#icon-profile-male'} />
+            </svg>
+            <p>6 rooms</p>
+          </div>
+          <div className="home__area">
+            <svg>
+              <use href={sprite + '#icon-expand'} />
+            </svg>
+            <p>
+              450 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <svg>
+              <use href={sprite + '#icon-key'} />
+            </svg>
+            <p>$2,200,000</p>
+          </div>
+          <button className="cta">Contact realtor</button>
+        </div>
+        {/* end home 2 */}
+        <div className="home">
+          <img
+            src={require('../images/house-3.jpeg')}
+            alt="House 3"
+            className="home__img"
+          />
+          <svg className="home__like">
+            <use href={sprite + '#icon-heart-full'} />
+          </svg>
+          <h5 className="home__name">Cozy Country House</h5>
+          <div className="home__location">
+            <svg>
+              <use href={sprite + '#icon-map-pin'} />
+            </svg>
+            <p>UK</p>
+          </div>
+          <div className="home__rooms">
+            <svg>
+              <use href={sprite + '#icon-profile-male'} />
+            </svg>
+            <p>4 rooms</p>
+          </div>
+          <div className="home__area">
+            <svg>
+              <use href={sprite + '#icon-expand'} />
+            </svg>
+            <p>
+              200 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <svg>
+              <use href={sprite + '#icon-key'} />
+            </svg>
+            <p>$800,000</p>
+          </div>
+          <button className="cta">Contact realtor</button>
+        </div>
+        {/* end home 3 */}
+        <div className="home">
+          <img
+            src={require('../images/house-4.jpeg')}
+            alt="House 4"
+            className="home__img"
+          />
+          <svg className="home__like">
+            <use href={sprite + '#icon-heart-full'} />
+          </svg>
+          <h5 className="home__name">Beautiful Family House</h5>
+          <div className="home__location">
+            <svg>
+              <use href={sprite + '#icon-map-pin'} />
+            </svg>
+            <p>USA</p>
+          </div>
+          <div className="home__rooms">
+            <svg>
+              <use href={sprite + '#icon-profile-male'} />
+            </svg>
+            <p>5 rooms</p>
+          </div>
+          <div className="home__area">
+            <svg>
+              <use href={sprite + '#icon-expand'} />
+            </svg>
+            <p>
+              325 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <svg>
+              <use href={sprite + '#icon-key'} />
+            </svg>
+            <p>$1,200,000</p>
+          </div>
+          <button className="cta">Contact realtor</button>
+        </div>
+        {/* end home 4 */}
+        <div className="home">
+          <img
+            src={require('../images/house-5.jpeg')}
+            alt="House 5"
+            className="home__img"
+          />
+          <svg className="home__like">
+            <use href={sprite + '#icon-heart-full'} />
+          </svg>
+          <h5 className="home__name">Beautiful Family House</h5>
+          <div className="home__location">
+            <svg>
+              <use href={sprite + '#icon-map-pin'} />
+            </svg>
+            <p>USA</p>
+          </div>
+          <div className="home__rooms">
+            <svg>
+              <use href={sprite + '#icon-profile-male'} />
+            </svg>
+            <p>5 rooms</p>
+          </div>
+          <div className="home__area">
+            <svg>
+              <use href={sprite + '#icon-expand'} />
+            </svg>
+            <p>
+              325 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <svg>
+              <use href={sprite + '#icon-key'} />
+            </svg>
+            <p>$1,200,000</p>
+          </div>
+          <button className="cta">Contact realtor</button>
+        </div>
+        {/* end home 5 */}
+        <div className="home">
+          <img
+            src={require('../images/house-6.jpeg')}
+            alt="House 6"
+            className="home__img"
+          />
+          <svg className="home__like">
+            <use href={sprite + '#icon-heart-full'} />
+          </svg>
+          <h5 className="home__name">Beautiful Family House</h5>
+          <div className="home__location">
+            <svg>
+              <use href={sprite + '#icon-map-pin'} />
+            </svg>
+            <p>USA</p>
+          </div>
+          <div className="home__rooms">
+            <svg>
+              <use href={sprite + '#icon-profile-male'} />
+            </svg>
+            <p>5 rooms</p>
+          </div>
+          <div className="home__area">
+            <svg>
+              <use href={sprite + '#icon-expand'} />
+            </svg>
+            <p>
+              325 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <svg>
+              <use href={sprite + '#icon-key'} />
+            </svg>
+            <p>$1,200,000</p>
+          </div>
+          <button className="cta">Contact realtor</button>
+        </div>
+        {/* end home 6 */}
+      </section>
       <section className="gallery">Gallery</section>
       <footer className="footer">Footer</footer>
     </>
